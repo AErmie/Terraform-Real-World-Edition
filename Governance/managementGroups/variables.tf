5f91c836b6aa2691565eb13d8a2e4c1da0ee97b4
Adin Ermie <Adin.Ermie@outlook.com>
2020-07-25 21:40:11 -0400

    MgmtGroup code semi-working (permissions issue)

--- Governance/managementGroups/variables.tf
+++ Governance/managementGroups/variables.tf
@@ -12,19 +12,20 @@ variable "tenant_id" {
 }
 
 variable "ParentMG-Name" {
-  type        = "string"
+  type        = string
   description = "This is the new top-level Management Group for all Subscriptions"
 }
 variable "ParentMG-SubIDs" {
-  type        = "list"
+  type        = list(string)
   description = "List all the Azure Subscription IDs that will be associated at this Management Group level (NOT any lower-level Management Groups)."
 }
 
 variable "ChildMG-Name" {
-  type        = "string"
+  type        = string
   description = "This is the new second-level Management Group to contain all Child Subscriptions"
 }
-variable "ChildMG-SubIDs" {
-  type        = "list"
+variable "ChildMG_SubIDs" {
+  type        = list(string)
   description = "List all the Azure Subscription IDs that will be associated at this Management Group level (NOT any lower-level Management Groups)."
+  default = []
 }
