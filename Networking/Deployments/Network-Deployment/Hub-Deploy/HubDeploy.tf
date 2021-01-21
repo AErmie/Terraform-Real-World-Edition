bb2f69d9cd42513f3e8a8ea19b4ab98439159f16
Adin Ermie <Adin.Ermie@outlook.com>
2021-01-21 08:49:22 -0500

    Testing Checkov with -T

--- Networking/Deployments/Network-Deployment/Hub-Deploy/HubDeploy.tf
+++ Networking/Deployments/Network-Deployment/Hub-Deploy/HubDeploy.tf
@@ -1,13 +1,13 @@
 # Allows what version of Terraform to use.
 terraform {
-  required_version = ">=0.12.0"
+  required_version = ">=0.14.0"
   # Backend for configuring remote state files to Azure Storage
-  backend "azurerm" {
+  # backend "azurerm" {
     # resource_group_name   = "TerraformStateRG"
     # storage_account_name  = "terraformstatesaae"
     # container_name        = "tfstate"
     # key                   = "networking_hub.tfstate"
-  }
+  # }
 }
 
 # Configure the Azure Provider
@@ -15,12 +15,12 @@ provider "azurerm" {
   # While version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = ">= 2.0.0"
   #(Optional) The Subscription ID which should be used. This can also be sourced from the ARM_SUBSCRIPTION_ID Environment Variable.
-  subscription_id = var.subscription_id
+  # subscription_id = var.subscription_id
   #(Optional) The Client ID which should be used. This can also be sourced from the ARM_CLIENT_ID Environment Variable.
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
   #(Optional) The Tenant ID which should be used. This can also be sourced from the ARM_TENANT_ID Environment Variable.
-  tenant_id       = var.tenant_id
+  # tenant_id       = var.tenant_id
   #(Optional) The Cloud Environment which should be used. Possible values are public, usgovernment, german and china. Defaults to public. This can also be sourced from the ARM_ENVIRONMENT environment variable.
   environment = "public"
   features {}
