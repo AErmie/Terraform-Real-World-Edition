597b57f7fce05c603e5bd9a1f54e316c45138a5e
Adin Ermie <Adin.Ermie@outlook.com>
2021-01-31 21:52:29 -0500

    Removed Subscription variables

--- Networking/Deployments/Network-Deployment/Hub-Deploy/variables.tf
+++ Networking/Deployments/Network-Deployment/Hub-Deploy/variables.tf
@@ -1,15 +1,15 @@
-variable "subscription_id" {
-    description = "Subscription ID"
-}
-variable "client_id" {
-    description = "Client ID"
-}
-variable "client_secret" {
-    description = "Client Secret"
-}
-variable "tenant_id" {
-    description = "Tenant ID"
-}
+# variable "subscription_id" {
+#     description = "Subscription ID"
+# }
+# variable "client_id" {
+#     description = "Client ID"
+# }
+# variable "client_secret" {
+#     description = "Client Secret"
+# }
+# variable "tenant_id" {
+#     description = "Tenant ID"
+# }
 
 
 variable "SharedServicesResourceGroupLocation" {
